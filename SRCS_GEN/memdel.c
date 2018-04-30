a3f3020d3b1927410b1b228b27642c8bc201f2b5
Theo PACALY <tpacaly@e2r8p21.42.fr>
2018-04-30 20:50:58 +0200

    g

--- SRCS_GEN/memdel.c
+++
@@ -1,9 +0,0 @@
-#include "../includes/corewar.h"
-
-void	ft_memdel(void **ap)
-{
-	if (ap == NULL)
-		return ;
-	free(*ap);
-	*ap = NULL;
-}
--- SRCS_GEN/strdel.c
+++
@@ -1,9 +0,0 @@
-#include "../includes/corewar.h"
-
-void	ft_strdel(char **as)
-{
-	if (as == NULL)
-		return ;
-	free(*as);
-	*as = NULL;
-}
